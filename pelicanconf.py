018f88daf5cf2ac197272dfc334fe78160e93400
Pavel Suchman <pavel@noopsware.com>
2013-12-22 01:43:07 +0200

    Blog - initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Pavel Suchman'
+SITENAME = u'Pavel on DevOps'
+SITEURL = ''
+
+TIMEZONE = 'Asia/Jerusalem'
+DISQUS_SITENAME = "pavelondevops"
+DEFAULT_LANG = u'en'
+DEFAULT_DATE = 'fs'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('Pelican', 'http://getpelican.com/'),
+          ('Leonid Mirsky', 'https://github.com/leonidlm/leonidlm.github.io'),
+          )
+
+# Social widget
+SOCIAL = (('twitter', 'http://twitter.com/pavelsuchman'),
+          ('github', 'http://github.com/flypunk'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
